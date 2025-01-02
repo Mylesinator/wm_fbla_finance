bea4ddde052460b0b17061144de5253668e37347
LazyKotaa <mylesmarshall06@gmail.com>
2025-01-02 16:41:30 -0700

    init express

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const clientPath = path.join(__dirname, "..", "client");
+
+const app = express();
+app.use(express.static(clientPath));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile("pages/index.html", { root: clientPath });
+});
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}/`);
+});
