797b0a78bfc2e2fb0dd1923c902be573a8130755
Nooneyouknowof <octaviomcnaughton@gmail.com>
2025-01-03 00:22:02 -0700

    index updates

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,12 +1,16 @@
 function form(element, method) {
-    element.addEventListener("submit", e => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const data = Object.fromEntries(formData.entries());
-        fetch(e.target.action, {
-            method: method,
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data)
+    try {
+        element.addEventListener("submit", e => {
+            e.preventDefault();
+            const formData = new FormData(e.target);
+            const data = Object.fromEntries(formData.entries());
+            fetch(e.target.action, {
+                method: method,
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data)
+            });
         });
-    });
+    } catch (error) {
+        alert(error.message);
+    }
 }
