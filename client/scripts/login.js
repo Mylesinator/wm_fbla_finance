942b0418c82a8c60baaba1b5daa3c4ffbd21003a
Mylesinator <mylesmarshall06@gmail.com>
2025-01-05 23:40:06 -0700

    fixed some skibidi & toilets

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -1,17 +1,22 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const login_form = document.getElementById("login-id");
+
     try {
         login_form.addEventListener("submit", async e => {
             e.preventDefault();
+
             const formData = new FormData(e.target);
             const data = Object.fromEntries(formData.entries());
+
             let response = await fetch(e.target.action, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(data)
             });
+
             let text = await response.text();
-            if (text != "User was not found!") {
+
+            if (response.ok) {
                 localStorage.setItem("auth", text);
                 window.location = "/";
             }
