ef6d59c4dbb5f10d7445f0592e29bfb706332d30
Nooneyouknowof <octaviomcnaughton@gmail.com>
2025-01-15 11:13:52 -0700

    1/15/25

--- client/scripts/register.js
+++ client/scripts/register.js
@@ -1,4 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     const signup_form = document.getElementById("signup-id");
-    form(signup_form, "POST");
+
+    signup_form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        try {
+            const formData = new FormData(e.target);
+            const data = Object.fromEntries(formData.entries());
+
+            let response = await fetch(`/users/signup`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data)
+            });
+
+            if (response.ok) {
+                window.location = "/login";
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    });
 });
